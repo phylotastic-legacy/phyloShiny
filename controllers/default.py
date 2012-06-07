24dc4e94d7faffcbf5e48242db48a4b2fa25c2ba
Chris Baron <cbaron@fieldmuseum.org>
2012-06-07 09:32:25 -0400

    Moved static html files to web2py view folders and updated references in these files to use the web2py URL function

--- controllers/default.py
+++ controllers/default.py
@@ -14,8 +14,7 @@ def index():
     example action using the internationalization operator T and flash
     rendered by views/default/index.html or views/generic.html
     """
-    response.flash = "Welcome to web2py!"
-    return dict(message=T('Hello World'))
+    return dict()
 
 def user():
     """
